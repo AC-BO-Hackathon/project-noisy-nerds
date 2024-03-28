f6c4ae304bbba4495eaae494e060160fcb169a06
Brenden Pelkie <bgpelkie@gmail.com>
2024-03-28 08:18:46 -0700

    baybe with karims framework

--- run_experiment_baybe.py
+++ run_experiment_baybe.py
@@ -47,72 +47,6 @@ MC_SAMPLES = 128 if not SMOKE_TEST else 16
 batch_size = 1
 
 
-# %%
-def generate_initial_data(problem, n: int, bounds: torch.Tensor) -> tuple:
-    X_init = draw_sobol_samples(
-        bounds=bounds, n=n, q=1, seed=torch.randint(100000, (1,)).item()
-    ).squeeze(-1)
-    Y_init = torch.tensor(problem.y(X_init.numpy()))
-    Y_init_real = torch.tensor(problem.f(X_init.numpy()))
-    return X_init, Y_init, Y_init_real
-
-# %%
-def initialize_model(train_x, train_y, noise_bool=True) -> tuple:
-    # define models for objective and constraint
-    train_y= -train_y  # negative because botorch assumes maximization
-
-    if noise_bool:
-        model = SingleTaskGP(
-            train_X=train_x,
-            train_Y=train_y.unsqueeze(-1),
-            outcome_transform=Standardize(m=1),
-        )
-    else:
-        model = SingleTaskGP(
-            train_X=train_x,
-            train_Y=train_y.unsqueeze(-1),
-            train_Yvar=torch.full_like(train_y.unsqueeze(-1), 1e-6),
-            outcome_transform=Standardize(m=1),
-        )
-
-    mll = ExactMarginalLogLikelihood(model.likelihood, model)
-
-    return mll, model
-
-# %%
-def optimize_acqf_loop(problem, acq_func):
-
-    standard_bounds = torch.zeros(2, problem.n_var, **tkwargs)
-    standard_bounds[1] = 1
-    options = {"batch_limit": batch_size, "maxiter": 2000}
-
-    while options["batch_limit"] >= 1:
-        try:
-            torch.cuda.empty_cache()
-            x_cand, acq_val = optimize_acqf(
-                acq_function=acq_func,
-                bounds=standard_bounds,
-                q=batch_size,
-                num_restarts=NUM_RESTARTS,
-                raw_samples=RAW_SAMPLES,  # used for intialization heuristic
-                options=options,
-            )
-            torch.cuda.empty_cache()
-            gc.collect()
-            break
-        except RuntimeError as e:
-            if options["batch_limit"] > 1:
-                print(
-                    "Got a RuntimeError in `optimize_acqf`. "
-                    "Trying with reduced `batch_limit`."
-                )
-                options["batch_limit"] //= 2
-                continue
-            else:
-                raise e
-
-    return x_cand, acq_val
-
 
 # %%
 from botorch.utils.sampling import draw_sobol_samples
@@ -172,14 +106,30 @@ def run_experiment(n_init, noise_level, budget, seed, noise_bool):
 
     measurements = optimization_campaign.measurements
 
-    y_real = np.
-    
+    # get X and noisy y values
+    x_names = [f'schwefel{i+1}' for i in range(N_DIMS_SCHWEF)]
+    x_train = measurements[x_names].to_numpy()
+    y_train = measurements['schwefel'].to_numpy()
+
+    # compile noise-free ground truth vals
+    y_real_complete = np.zeros(len(y_init_real) + len(y_real))
+
+    for i, val in enumerate(y_init_real):
+        y_real_complete[i] = val
+
+    for i, val in enumerate(y_real):
+        y_real_complete[i+len(y_init_real)] = val
     
     os.makedirs('results', exist_ok=True)
     fname = f"results/{problem.__class__.__name__[:5]}_n_init_{n_init}_noiselvl_{noise_level}_budget_{budget}_seed_{seed}_noise_{noise_bool}.pt"
     torch.save((train_X, train_Y, train_Y_real, model), fname)
+
+    train_X = torch.from_numpy(x_train)
+    train_Y = torch.from_numpy(y_train)
+    train_Y_real = torch.from_numpy(y_real_complete)
     
-    return train_X, train_Y, train_Y_real, model
+    return train_X, train_Y, train_Y_real, None
+
         
         
 if __name__ == "__main__":
